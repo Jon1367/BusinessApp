0ef6b8f07d9ed613871cfb3d8d394a3a2794286f
root <jonathanO1367@yahoo.com>
2015-10-30 00:33:47 -0400

    created new view to display places

--- public/js/location.js
+++ public/js/location.js
@@ -15,3 +15,26 @@ $.ajax({
     x.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;  
 }
+
+
+
+/* This pen is copyrighted by Intert 2015. All rights reserved. No reproduction authorized */
+
+$(document).ready( function() {
+  $('.cookies-used').fadeIn();
+  setEvents();
+});
+
+setEvents = function() {
+  $('.black-complete-background').click( function() {
+    $('.password-reset-div').fadeOut();
+  });
+}
+
+showPasswordForgotten = function() {
+  $('.password-reset-div').fadeIn();
+}
+
+closePF = function() {
+   $('.password-reset-div').fadeOut();
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,65 +1,32 @@
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>The Business Network</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="/css/style.css">
 
-
-
+<link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
     </head>
     <body>
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand" href="#">The Business Network</a>
     </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
+      <form class="navbar-form navbar-right" role="search">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
+
+          <input type="text" class="form-control" placeholder="username">
+          <input type="text" class="form-control" placeholder="password">
+
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav> 
@@ -70,22 +37,156 @@
 
 
 
-      <div class="container">
-        <h1>Hello, world!</h1>
-        <div id="demo">
-            
-        </div>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+
+ <div class="row blackBG">
+
+
+		<h1 class="text-center whiteFont">Welcome to Business Network</h1>
+		<div class="col-md-6 col-md-offset-3">
+		<br>
+			<p class="whiteFont" >&#09;  This is website/app that allows Users to check out similar 
+		businesses on a mutual platform for a variety of motives such as
+		 networking, knowledge of what the businesses have to offer and
+		  the businesses financials  so that ultimately it allows the user 
+		  to choose what business fits their need most. </p>
+<br>
+	</div>
+  </div>
+
+<br>
+<br>
+
+  <div class="row">
+    <div class="col-md-4 col-md-push-1">
+    <h3 class="text-center">Search for nearby business</h3>
+<br>
+
+
+    <ul class="text-center">
+      <li>
+        <p>- Search for business near your area to see their business model and how the company financials.</p>
+      </li>
+      <li>
+        <p>Sign up to collaborate with other business owners.</p>
+      </li>
+    </ul>
+      
+    </div>
+    <div class="col-md-4 col-md-push-2">
+      <h3 class="text-center">Test it Out</h3>
+
+<form class="form-horizontal" role="form">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Business Name</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" id="email" placeholder="Enter email">
       </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Business catergory</label>
+      <div class="col-sm-10">          
+       
+          <select class="form-control" id="pwd">
+    <option>Business catergory</option>
+    <option>Tech</option>
+    <option>Start Up</option>
+    <option>Food</option>
+    <option>marketing</option>
+    <option>small business</option>
+    <option>business near me</option>
+
+  </select>
+      </div>
+    </div>
+
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-lg  btn-primary">Submit</button>
+      </div>
+    </div>
+  </form>
+    </div>
+    
+  </div> <!-- end of second row -->
+<br>
+<br>
+          <!--Services -->
+<section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading HeaderColor whiteFont">Service</h2>
+                
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <span class="glyphicon glyphicon-stats"></span>
+                
+                    </span>
+                    <h4 class="service-heading">Research</h4>
+                    <p class="text-muted">Do research on what company on other conpanies.(public infomation only)</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <span class="glyphicon glyphicon-user"></span>
+             
+                    </span>
+                    <h4 class="service-heading">Collaborate</h4>
+                    <p class="text-muted"> Find other business willing to collaborate</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </span>
+                    <h4 class="service-heading">Discover</h4>
+                    <p class="text-muted">Find What other business are doing</p>
+                </div>
+            </div>
+    </div>
+</section>
+<br>
+<br>
+	
+
+  <div class="row">
+    <h1 class="text-center">Contact US</h1>
+    <br>
+    <br>
+<div class="col-md-4 col-md-push-1">
+
+<h1>The Business Network</h1>
+</div>
+    <div class="col-md-4 col-md-push-3">
+
+    <ul>
+      
+      <li>  <p><b>e: </b> <a href="">Jonathano1367@gmail.com</a></p></li>
+      <li>  <p><b>p: </b>  <a href="">1(310)717-30</a></p></li>
+      <li>  <p><b>adress: </b> <a href="">14211 Ocean Gate ave, Hawthorne, 32792, CA</a></p></li>
+
+
+    </ul>
+    <br>
+      
+    </div>
+  </div>
+
+
+
     
 
-
+<footer></footer>
 
 
    </body>
+   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="/js/location.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-material/0.11.4/angular-material.js"></script>
+
 
 </html>
